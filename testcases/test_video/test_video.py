75bf020974ffbd45231bc61d5fcfc2904c556e44
何方 <he529564582@163.com>
2024-06-25 16:16:06 +0800

    fear:add new video testcase

--- testcases/test_video/test_video.py
+++ testcases/test_video/test_video.py
@@ -70,3 +70,126 @@ class TestVideo:
 		except Exception as e:
 			logger.error(f"Request failed: {e}")
 			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_group_list.yaml')))
+	def test_video_group_list(self, args, session):
+		url = args['request']['url']
+		logger.info(f"Testing video group list with URL: {url}")
+		res = session.get(url)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_category_list.yaml')))
+	def test_video_category_list(self, args, session):
+		url = args['request']['url']
+		logger.info(f"Testing video category list with URL: {url}")
+		res = session.get(url)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_group.yaml')))
+	def test_video_group(self, args, session):
+		url = args['request']['url']
+		params = args['request']['params']
+		logger.info(f"Testing video group with URL: {url} and params: {params}")
+		res = session.get(url, params=params)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_all.yaml')))
+	def test_video_all(self, args, session):
+		url = args['request']['url']
+		params = args['request'].get('params')
+		if params is None:
+			res = session.get(url)
+			logger.info(f"Testing video all with URL: {url}")
+		else:
+			res = session.get(url, params=params)
+			logger.info(f"Testing video all with URL: {url} and params: {params}")
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_recommend.yaml')))
+	def test_video_recommend(self, args, session):
+		url = args['request']['url']
+		params = args['request'].get('params')
+		if params is None:
+			res = session.get(url)
+			logger.info(f"Testing video all with URL: {url}")
+		else:
+			res = session.get(url, params=params)
+			logger.info(f"Testing video all with URL: {url} and params: {params}")
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_related.yaml')))
+	def test_video_related(self, args, session):
+		url = args['request']['url']
+		params = args['request']['params']
+		logger.info(f"Testing video related with URL: {url} and params: {params}")
+		res = session.get(url, params=params)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_detail.yaml')))
+	def test_video_detail(self, args, session):
+		url = args['request']['url']
+		params = args['request']['params']
+		logger.info(f"Testing video detail with URL: {url} and params: {params}")
+		res = session.get(url, params=params)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_detail_info.yaml')))
+	def test_video_detail_info(self, args, session):
+		url = args['request']['url']
+		params = args['request']['params']
+		logger.info(f"Testing video detail info with URL: {url} and p arams: {params}")
+		res = session.get(url, params=params)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
+	
+	@pytest.mark.parametrize('args', utils.read_file(os.path.join(os.getcwd(), 'data', 'video', 'video_url.yaml')))
+	def test_video_url(self, args, session):
+		url = args['request']['url']
+		params = args['request']['params']
+		logger.info(f"Testing video url with URL: {url} and params: {params}")
+		res = session.get(url, params=params)
+		try:
+			res.raise_for_status()
+			logger.info(f"Response: {res.json()}")
+		except Exception as e:
+			logger.error(f"Request failed: {e}")
+			raise
